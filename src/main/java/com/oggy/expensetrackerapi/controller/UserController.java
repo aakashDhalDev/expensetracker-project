74eeaec6b7422a358e31274387fd218a36382884
Aakash Dhal <134286523+aakashDhalDev@users.noreply.github.com>
2025-03-16 14:39:05 -0700

    Implemented a stateless authentication using JWT tokens.

--- src/main/java/com/oggy/expensetrackerapi/controller/UserController.java
+++ src/main/java/com/oggy/expensetrackerapi/controller/UserController.java
@@ -2,18 +2,28 @@ package com.oggy.expensetrackerapi.controller;
 
 import com.oggy.expensetrackerapi.entity.User;
 import com.oggy.expensetrackerapi.entity.UserModel;
+import com.oggy.expensetrackerapi.service.JwtService;
 import com.oggy.expensetrackerapi.service.UserService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-public class UserController {
+public class  UserController {
 
     @Autowired
     private UserService userService;
+
+    @Autowired
+    AuthenticationManager authenticationManager;
+
+    @Autowired
+    private JwtService jwtService;
 
     @PostMapping("/register")
     public ResponseEntity<User> save(@Valid @RequestBody UserModel user){
@@ -34,5 +44,17 @@ public class UserController {
     public ResponseEntity<HttpStatus> deleteUser(@PathVariable Long id){
         userService.deleteUser(id);
         return new ResponseEntity<HttpStatus>(HttpStatus.NO_CONTENT);
+    }
+
+    @PostMapping("/login")
+    public String login(@RequestBody UserModel user){
+
+        Authentication authentication = authenticationManager
+                .authenticate(new UsernamePasswordAuthenticationToken(user.getEmail(), user.getPassword()));
+
+        if(authentication.isAuthenticated())
+            return jwtService.generateToken(user.getEmail());
+        else
+            return "Login Failure :(";
     }
 }
